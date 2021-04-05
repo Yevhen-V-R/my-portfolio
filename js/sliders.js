4e714615eff2336b73056f1fc53405a253432d42
Yevhen <yevgenyr@yahoo.com>
2021-04-06 00:30:35 +0300

    Add all project files

---
+++ js/sliders.js
@@ -0,0 +1,32 @@
+// Connect & Initialize Swiper slider library 
+// ..................................
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  // ........................
+  // direction: 'vertical',
+  // loop: true,
+
+  // set up parameters for slider
+  speed: 1000,
+  effect: 'slide',
+  grabCursor: true,
+
+  // If we need pagination
+  // ........................
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  // ........................
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  // ........................
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
